10ac5faba09bb98744efa7736bccae9fbf706ebb
ashishpathak981209-sketch <ashishpathak981209@gmail.com>
2026-02-14 12:46:09 +0530

    ashisj
    error  128

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,10 @@
 @echo off
+git pull
 
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--lowvram --xformers --no-half-vae --medvram-sdxl --autolaunch --opt-channelslast
+
 
 call webui.bat
